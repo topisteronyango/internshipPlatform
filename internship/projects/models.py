6dd4bc8fb75b90d2f81bef4d9ceaead79eb4eedb
t-onyango <t.onyango@alustudent.com>
2023-06-23 21:54:15 +0300

    added content

--- internship/projects/models.py
+++ internship/projects/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 # Create your models here.
@@ -36,3 +38,12 @@ class Project(models.Model):
     description = models.TextField(blank=True)
     def __str__(self):
         return self.title
+
+class Content(models.Model):
+    project = models.ForeignKey(Project,
+    related_name='contents',
+    on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,
+    on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
