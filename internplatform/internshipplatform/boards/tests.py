1cad8440f101c17f25d1e109f3651edf35daa014
t-onyango <t.onyango@alustudent.com>
2023-07-12 20:14:41 +0300

    testing

--- internplatform/internshipplatform/boards/tests.py
+++ internplatform/internshipplatform/boards/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
+
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
