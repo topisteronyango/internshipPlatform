0b05131ba1702b2d966b57ff236a29f82e68a66a
topisteronyango <t.onyango@alustudent.com>
2023-07-28 16:53:24 +0300

    Delete internship directory

--- internship/students/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-from projects.models import Specialization
-class SpecializationEnrollForm(forms.Form):
-    specialization = forms.ModelChoiceField(queryset=Specialization.objects.all(), widget=forms.HiddenInput)
--- internship/students/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
- path('register/', views.StudentRegistrationView.as_view(), name='student_registration'),
- path('enroll-specialization/', views.StudentEnrollSpecializationView.as_view(), name='student_enroll_specialization'),
- path('specializations/', views.StudentSpecializationListView.as_view(), name='student_specialization_list'),
- path('specialization/<pk>/', views.StudentSpecializationDetailView.as_view(), name='student_specialization_detail'),
- path('specialization/<pk>/<project_id>/', views.StudentSpecializationDetailView.as_view(), name='student_specialization_detail_project'),
-]
