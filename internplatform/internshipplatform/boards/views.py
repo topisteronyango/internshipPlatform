a1de7fe1b6c2478ecacd1de17c73cf16eedd1212
topisteronyango <t.onyango@alustudent.com>
2023-07-28 16:54:22 +0300

    Delete internplatform directory

--- internplatform/internshipplatform/boards/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Board
-# Create your views here.
-
-def home(request):
-    boards = Board.objects.all()
-    return render(request, 'home.html', {'boards':boards})
