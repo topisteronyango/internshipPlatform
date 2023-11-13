ed8bd6e4211a7285e32f43d0d9b1949c057a3bb7
topisteronyango <t.onyango@alustudent.com>
2023-11-13 11:29:25 +0200

    configure hosting

--- InternBoard/settings.py
+++ InternBoard/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = [
